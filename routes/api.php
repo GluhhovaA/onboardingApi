c1ab1a28f786b45b0726d420359f855a9c7e2d9b
aleksandraglukhova <glukhova.intern@macpaw.com>
2021-08-28 19:55:09 +0300

    delete route

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/users', 'Api\UserController@index');
-
 Route::post('/signin', 'Api\SigninController@signin');
 
 Route::get('/profile/user', 'Api\ProfileController@index');
