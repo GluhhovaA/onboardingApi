b8d4c2ad0113ca69fd95abaf0bff029eb830ebc5
aleksandraglukhova <glukhova.intern@macpaw.com>
2021-08-30 12:42:42 +0300

    add rules and messages for validation

--- app/Http/Requests/ProfileRequest.php
+++ app/Http/Requests/ProfileRequest.php
@@ -16,6 +16,21 @@ class ProfileRequest extends FormRequest
     {
         return [
             'photo' => 'mimes:jpeg,bmp,png,jpg',
+            'phone' => 'size:10',
+            'firstName' => 'max:20|min:2',
+            'lastName' => 'max:20|min:2',
+            'position' => 'max:20|min:5'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'phone' => 'Phone need to be 10 characters',
+            'photo' => 'Please, add photo',
+            'firstName' => 'Please, enter correct first name',
+            'lastName' => 'Please, enter correct last name',
+            'position' => 'Please, enter correct position'
         ];
     }
 }
