83226fb552c821aada4f28eb17a8ab4a895eaa48
aleksandraglukhova <glukhova.intern@macpaw.com>
2021-08-31 13:20:53 +0300

    cleanuo code

--- database/seeds/TaskSeed.php
+++ database/seeds/TaskSeed.php
@@ -32,7 +32,7 @@ class TaskSeed extends Seeder
         return [
             [
                 'title' => 'Getting equipment and a little present',
-                'content' => 'How to get and set up your laptop? Here is the guide: \nSend the following information to lavrenyuk@macpaw.com :\nyour full name \nyour address \nyour phone number \nReceive your laptop. \nFollow the instructions that will be shared with you once you get your MacBook.',
+                'content' => "How to get and set up your laptop? Here is the guide: \nSend the following information to lavrenyuk@macpaw.com :\nyour full name \nyour address \nyour phone number \nReceive your laptop. \nFollow the instructions that will be shared with you once you get your MacBook.",
                 'article_title' => '',
                 'article_content' => '',
                 'file' => '',
@@ -41,7 +41,7 @@ class TaskSeed extends Seeder
             ],
             [
                 'title' => 'Your first work day',
-                'content' => 'It’s time to start your onboarding process. \nHoover promises, it’ll be as great as a whole bowl of warm milk! \nQuick look into the future. \nOnboarding is ahead of you! Here you will find out who you have to work with and how to make your \nwork more efficient. Here you will hear the story of MacPaw and its projects, you will be able \nto ask CEO questions - and he will even answer! You will be told about cybersecurity, \ncharitable projects, and internal working tools. Onboarding is the time filled with useful and \ninteresting information. This is the time when MacPaw cares that your work at the company will be an enjoyable experience.\nInvitations to all the meetings and events will be sent on your email. Don’t forget to check it from time to time. \nAnd I guess that\'s your first day with MacPaw slack. To be in touch from everywhere, at any time you want \nto, and as easy as you always wanted, take a look at Google Calendar and Zoom apps. \nAnd you definitely will need Fix Bot to make great use of your fixes!',
+                'content' => "It’s time to start your onboarding process. \nHoover promises, it’ll be as great as a whole bowl of warm milk! \nQuick look into the future. \nOnboarding is ahead of you! Here you will find out who you have to work with and how to make your \nwork more efficient. Here you will hear the story of MacPaw and its projects, you will be able \nto ask CEO questions - and he will even answer! You will be told about cybersecurity, \ncharitable projects, and internal working tools. Onboarding is the time filled with useful and \ninteresting information. This is the time when MacPaw cares that your work at the company will be an enjoyable experience.\nInvitations to all the meetings and events will be sent on your email. Don’t forget to check it from time to time. \nAnd I guess that\'s your first day with MacPaw slack. To be in touch from everywhere, at any time you want \nto, and as easy as you always wanted, take a look at Google Calendar and Zoom apps. \nAnd you definitely will need Fix Bot to make great use of your fixes!",
                 'article_title' => '',
                 'article_content' => '',
                 'file' => '',
@@ -59,7 +59,7 @@ class TaskSeed extends Seeder
             ],
             [
                 'title' => 'Here we’ll be all the info from the Security Team that newcomers need to know/go through.',
-                'content' => 'Hello 🖖 \nYou have been signed up for a security awareness training - Mandatory. Security Awareness Fundamentals Trainings. \nPlease complete your training by 03/09/2021 23:59. \nAll the training sessions are in the Knowbe4, the link you’ll find down below. You can also login to the training portal via Knowbe4 app in your Okta account. \nClick the Start button on your training page to begin your training. \nNOTE: To make sure that your progress is recorded, please close the window when you are done with your training. When you are ready to continue, you will be able to start your training from where you left off. \nHave fun and stay secure ❤ \nMacPaw Security Team',
+                'content' => "Hello 🖖 \nYou have been signed up for a security awareness training - Mandatory. Security Awareness Fundamentals Trainings. \nPlease complete your training by 03/09/2021 23:59. \nAll the training sessions are in the Knowbe4, the link you’ll find down below. You can also login to the training portal via Knowbe4 app in your Okta account. \nClick the Start button on your training page to begin your training. \nNOTE: To make sure that your progress is recorded, please close the window when you are done with your training. When you are ready to continue, you will be able to start your training from where you left off. \nHave fun and stay secure ❤ \nMacPaw Security Team",
                 'article_title' => '',
                 'article_content' => '',
                 'file' => '',
@@ -69,7 +69,7 @@ class TaskSeed extends Seeder
             [
                 'title' => 'Feedback workshop',
                 'content' => 'Here we’ll put a link to a Zoom meeting, notes from the lecture and a presentation. It’d help to keep all the information at hand.',
-                'article_content' => 'Here I collected tips from our workshop 😊 \nFeedback is a gift. It can be \npositive; \nconstructive - what to improve; \nuneffective. \nSigns of  bad feedback: \nWhen you give it in a group of other people. \nOr give it emotionally. \nWhen it’s unexpected. \nOr given at a bad time. \nWhen it feels like an attack 🦁 \nOr the feedback that you decided to keep to yourself and never voice out. ',
+                'article_content' => "Here I collected tips from our workshop 😊 \nFeedback is a gift. It can be \npositive; \nconstructive - what to improve; \nuneffective. \nSigns of  bad feedback: \nWhen you give it in a group of other people. \nOr give it emotionally. \nWhen it’s unexpected. \nOr given at a bad time. \nWhen it feels like an attack 🦁 \nOr the feedback that you decided to keep to yourself and never voice out. ",
                 'article_title' => '',
                 'article_content' => '',
                 'file' => '',
