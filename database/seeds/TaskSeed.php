b60bace0f8c1792c224883bcd3da37b0af6c4640
aleksandraglukhova <glukhova.intern@macpaw.com>
2021-08-31 15:05:57 +0300

    cleanup code

--- database/seeds/TaskSeed.php
+++ database/seeds/TaskSeed.php
@@ -35,8 +35,8 @@ class TaskSeed extends Seeder
                 'content' => "How to get and set up your laptop? Here is the guide: \nSend the following information to lavrenyuk@macpaw.com :\nyour full name \nyour address \nyour phone number \nReceive your laptop. \nFollow the instructions that will be shared with you once you get your MacBook.",
                 'article_title' => mb_strimwidth('Getting equipment and a little present', 0, 50),
                 'article_content' => mb_strimwidth("How to get and set up your laptop? Here is the guide: \nSend the following information to lavrenyuk@macpaw.com :\nyour full name \nyour address \nyour phone number \nReceive your laptop. \nFollow the instructions that will be shared with you once you get your MacBook.", 0, 150, '...'),
-                'file' => '',
-                'link' => '',
+                'file' => 'Some file',
+                'link' => 'Some link',
                 'status' => true
             ],
             [
@@ -44,8 +44,8 @@ class TaskSeed extends Seeder
                 'content' => "It’s time to start your onboarding process. \nHoover promises, it’ll be as great as a whole bowl of warm milk! \nQuick look into the future. \nOnboarding is ahead of you! Here you will find out who you have to work with and how to make your \nwork more efficient. Here you will hear the story of MacPaw and its projects, you will be able \nto ask CEO questions - and he will even answer! You will be told about cybersecurity, \ncharitable projects, and internal working tools. Onboarding is the time filled with useful and \ninteresting information. This is the time when MacPaw cares that your work at the company will be an enjoyable experience.\nInvitations to all the meetings and events will be sent on your email. Don’t forget to check it from time to time. \nAnd I guess that\'s your first day with MacPaw slack. To be in touch from everywhere, at any time you want \nto, and as easy as you always wanted, take a look at Google Calendar and Zoom apps. \nAnd you definitely will need Fix Bot to make great use of your fixes!",
                 'article_title' => mb_strimwidth('Your first work day', 0, 50),
                 'article_content' => mb_strimwidth("It’s time to start your onboarding process. \nHoover promises, it’ll be as great as a whole bowl of warm milk! \nQuick look into the future. \nOnboarding is ahead of you! Here you will find out who you have to work with and how to make your \nwork more efficient. Here you will hear the story of MacPaw and its projects, you will be able \nto ask CEO questions - and he will even answer! You will be told about cybersecurity, \ncharitable projects, and internal working tools. Onboarding is the time filled with useful and \ninteresting information. This is the time when MacPaw cares that your work at the company will be an enjoyable experience.\nInvitations to all the meetings and events will be sent on your email. Don’t forget to check it from time to time. \nAnd I guess that\'s your first day with MacPaw slack. To be in touch from everywhere, at any time you want \nto, and as easy as you always wanted, take a look at Google Calendar and Zoom apps. \nAnd you definitely will need Fix Bot to make great use of your fixes!", 0, 150, '...'),
-                'file' => '',
-                'link' => '',
+                'file' => 'Some file',
+                'link' => 'Some link',
                 'status' => true
             ],
             [
@@ -53,8 +53,8 @@ class TaskSeed extends Seeder
                 'content' => 'Here you’ll get to know other Macpawians and your buddy.',
                 'article_title' => mb_strimwidth('Meeting with the team & your buddy', 0, 50),
                 'article_content' => mb_strimwidth('Here you’ll get to know other Macpawians and your buddy.', 0, 150),
-                'file' => '',
-                'link' => '',
+                'file' => 'Some file',
+                'link' => 'Some link',
                 'status' => true
             ],
             [
@@ -62,8 +62,8 @@ class TaskSeed extends Seeder
                 'content' => "Hello 🖖 \nYou have been signed up for a security awareness training - Mandatory. Security Awareness Fundamentals Trainings. \nPlease complete your training by 03/09/2021 23:59. \nAll the training sessions are in the Knowbe4, the link you’ll find down below. You can also login to the training portal via Knowbe4 app in your Okta account. \nClick the Start button on your training page to begin your training. \nNOTE: To make sure that your progress is recorded, please close the window when you are done with your training. When you are ready to continue, you will be able to start your training from where you left off. \nHave fun and stay secure ❤ \nMacPaw Security Team",
                 'article_title' => mb_strimwidth('Here we’ll be all the info from the Security Team that newcomers need to know/go through.', 0, 50, '...'),
                 'article_content' => mb_strimwidth("Hello 🖖 \nYou have been signed up for a security awareness training - Mandatory. Security Awareness Fundamentals Trainings. \nPlease complete your training by 03/09/2021 23:59. \nAll the training sessions are in the Knowbe4, the link you’ll find down below. You can also login to the training portal via Knowbe4 app in your Okta account. \nClick the Start button on your training page to begin your training. \nNOTE: To make sure that your progress is recorded, please close the window when you are done with your training. When you are ready to continue, you will be able to start your training from where you left off. \nHave fun and stay secure ❤ \nMacPaw Security Team", 0,150, '...'),
-                'file' => '',
-                'link' => 'All your training sessions in KnowBe4',
+                'file' => 'Some file',
+                'link' => 'Some link',
                 'status' => true
             ],
             [
@@ -71,7 +71,7 @@ class TaskSeed extends Seeder
                 'content' => 'Here we’ll put a link to a Zoom meeting, notes from the lecture and a presentation. It’d help to keep all the information at hand.',
                 'article_title' => 'Tips from our workshop',
                 'article_content' => mb_strimwidth("Here I collected tips from our workshop 😊 \nFeedback is a gift. It can be \npositive; \nconstructive - what to improve; \nuneffective. \nSigns of  bad feedback: \nWhen you give it in a group of other people. \nOr give it emotionally. \nWhen it’s unexpected. \nOr given at a bad time. \nWhen it feels like an attack 🦁 \nOr the feedback that you decided to keep to yourself and never voice out. ", 0, 150, '...'),
-                'file' => '',
+                'file' => 'Some file',
                 'link' => 'Article on Confluence about the Feedback',
                 'status' => true
             ],
